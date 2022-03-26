692119a3bd4daf08844f1183887b69f60982a0b5
semiexp <linthya.v@gmail.com>
2022-03-26 09:48:24 +0900

    Load PuzzleSolver directly in index.tsx

--- packages/cspuz-solver2/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { PuzzleSolver } from "./PuzzleSolver";
-import React from "react";
-
-function App() {
-    return <PuzzleSolver />;
-}
-
-export default App;
--- packages/cspuz-solver2/src/index.tsx
+++ packages/cspuz-solver2/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { PuzzleSolver } from "./PuzzleSolver";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PuzzleSolver />
   </React.StrictMode>,
   document.getElementById('root')
 );
