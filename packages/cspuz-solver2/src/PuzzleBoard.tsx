21b53aece7627e9f0e62f7384e87247de5fb0f57
semiexp <linthya.v@gmail.com>
2022-05-15 11:00:52 +0900

    Support Tapa clues in the puzzle board

--- packages/cspuz-solver2/src/PuzzleBoard.tsx
+++ packages/cspuz-solver2/src/PuzzleBoard.tsx
@@ -39,7 +39,8 @@ export type Item =
     | "dottedHorizontalWall"
     | "dottedVerticalWall"
     | { kind: "text", data: string }
-    | { kind: "compass", up: number, down: number, left: number, right: number };
+    | { kind: "compass", up: number, down: number, left: number, right: number }
+    | { kind: "tapaClue", value: number[] };
 
 type RenderEnv = {
     offsetY: number,
@@ -134,6 +135,36 @@ function renderItem(env: RenderEnv, y: number, x: number, color: string, item: I
                         item.right >= 0 && <text x={centerX + unitSize * 0.3} y={centerY} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.4 }} fill={color}>{item.right}</text>
                     }
                 </g>
+            } else if (item.kind === "tapaClue") {
+                let values = [];
+                for (let i = 0; i < item.value.length; ++i) {
+                    if (item.value[i] === -2) {
+                        values.push("?");
+                    } else if (item.value[i] !== -1) {
+                        values.push(item.value[i] + "");
+                    }
+                }
+                if (values.length === 1) {
+                    return <text x={centerX} y={centerY} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.8 }} fill={color}>{values[0]}</text>;
+                } else if (values.length === 2) {
+                    return <g>
+                        <text x={centerX - unitSize * 0.2} y={centerY - unitSize * 0.2} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[0]}</text>
+                        <text x={centerX + unitSize * 0.2} y={centerY + unitSize * 0.2} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[1]}</text>
+                    </g>;
+                } else if (values.length === 3) {
+                    return <g>
+                        <text x={centerX - unitSize * 0.3} y={centerY - unitSize * 0.2} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[0]}</text>
+                        <text x={centerX + unitSize * 0.3} y={centerY - unitSize * 0.2} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[1]}</text>
+                        <text x={centerX} y={centerY + unitSize * 0.2} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[2]}</text>
+                    </g>;
+                } else if (values.length === 4) {
+                    return <g>
+                        <text x={centerX - unitSize * 0.3} y={centerY} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[0]}</text>
+                        <text x={centerX} y={centerY - unitSize * 0.25} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[1]}</text>
+                        <text x={centerX} y={centerY + unitSize * 0.25} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[2]}</text>
+                        <text x={centerX + unitSize * 0.3} y={centerY} dominantBaseline="central" textAnchor="middle" style={{ fontSize: unitSize * 0.6 }} fill={color}>{values[3]}</text>
+                    </g>;
+                }
             }
         }
         throw new Error("unsupported item: " + item);
