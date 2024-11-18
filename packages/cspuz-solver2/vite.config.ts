1d5eef8d99cc5b862d18ee46d9fa1606d93b9cd2
semiexp <linthya.v@gmail.com>
2024-11-18 20:29:07 +0900

    fix build output file names

--- packages/cspuz-solver2/vite.config.ts
+++ packages/cspuz-solver2/vite.config.ts
@@ -15,5 +15,23 @@ export default defineConfig({
       ]
     })
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      }
+    }
+  },
+  worker: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      }
+    }
+  },
   base: "./",
 });
