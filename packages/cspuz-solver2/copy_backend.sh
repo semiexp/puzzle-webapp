8bee0a3c03f60295eec94b40e2b4864631004c0a
semiexp <linthya.v@gmail.com>
2026-02-11 07:38:37 +0000

    copy numlin solver in copy_backend.sh

--- packages/cspuz-solver2/copy_backend.sh
+++ packages/cspuz-solver2/copy_backend.sh
@@ -4,3 +4,6 @@ cd $(dirname $0)
 
 rm -rf src/solver/cspuz
 cp -r ../../../cspuz_core/build/cspuz_solver_backend/ src/solver/cspuz
+
+rm -rf src/solver/numlin
+cp -r ../../../numlin/build/numlin_js/ src/solver/numlin
